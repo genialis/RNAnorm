578059f9c04b29b7ded12bdc2ddb01b6d027082d
Miha Stajdohar <miha.stajdohar@gmail.com>
2020-07-29 18:23:08 +0200

    Test tpm_normalization method on manually computed TPM expressions

--- tests/test_tpm_normalization.py
+++ tests/test_tpm_normalization.py
@@ -1,45 +1,43 @@
+import numpy as np
 import pandas as pd
 
 from rnanorm.normalization import tpm_normalization
 
 
 def test_tpm_normalization():
-    expressions = [[500, 400], [20000, 19000], [500, 600]]
+    """Test TPM formula implementation.
+
+    TPM expressions of a minimal example have been manually computed and
+    compared against implementation.
+
+    Expression data:
+
+                        S1     S2
+    ENSG00000136807    500    400
+    ENSG00000176903  20000  19000
+    ENSG00000241490    500    600
+
+    Gene lengths:
+
+    ENSG00000136807  3000
+    ENSG00000176903  2590
+    ENSG00000241490  3101
+
+    Manually computed TPM (rounded):
+
+                         S1      S2
+    ENSG00000136807   20704   17400
+    ENSG00000176903  959266  957349
+    ENSG00000241490   20029   25250
+    """
     genes = ["ENSG00000136807", "ENSG00000176903", "ENSG00000241490"]
     gene_lengths = [3000, 2590, 3101]
+    expressions = [[500, 400], [20000, 19000], [500, 600]]
+    manually_computed_TPM = [[20704, 17400], [959266, 957349], [20029, 25250]]
+
     X = pd.DataFrame(expressions, index=genes, columns=["S1", "S2"])
     y = pd.DataFrame(gene_lengths, index=genes, columns=["GENE_LENGTHS"])
-    s11 = X.loc["ENSG00000136807"]["S1"]
-    s12 = X.loc["ENSG00000176903"]["S1"]
-    s13 = X.loc["ENSG00000241490"]["S1"]
-    l1 = y.loc["ENSG00000136807"]["GENE_LENGTHS"]
-    l2 = y.loc["ENSG00000176903"]["GENE_LENGTHS"]
-    l3 = y.loc["ENSG00000241490"]["GENE_LENGTHS"]
-    ##################################################
-    # New implementation according with one web source
-    a11 = s11 / l1
-    a12 = s12 / l2
-    a13 = s13 / l3
-    a = a11 + a12 + a13
-    g11 = a11 / a * 1e6
-    g12 = a12 / a * 1e6
-    g13 = a13 / a * 1e6
-    print(g11)
-    print(g12)
-    print(g13)
-    print()
-    ################################################
-    # Janez'z formula picked from another web source
-    rpk1 = s11 * 1e3 / l1
-    rpk2 = s12 * 1e3 / l2
-    rpk3 = s13 * 1e3 / l3
-    rpk = rpk1 + rpk2 + rpk3
-    g11 = rpk1 / rpk * 1e6
-    g12 = rpk2 / rpk * 1e6
-    g13 = rpk3 / rpk * 1e6
-    print(g11)
-    print(g12)
-    print(g13)
-    print()
+
     TPM = tpm_normalization(X.to_numpy(), y.to_numpy())
-    print(TPM)
+
+    assert np.all(TPM.astype(int) == manually_computed_TPM)
