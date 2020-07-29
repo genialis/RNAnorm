ba9a9644e72d5151d9d59f2dd5570472067ac859
Andrej Copar <andrej@genialis.com>
2020-07-29 15:58:06 +0200

    Lint rnanorm module

--- src/rnanorm/normalization.py
+++ src/rnanorm/normalization.py
@@ -6,36 +6,36 @@ import numpy as np
 import pandas as pd
 
 
-def tpm_normalization(X, l):
+def tpm_normalization(X, y):
     """TPM normalization.
 
     :type X: Numpy ndarray
-    :type l: Numpy ndarray
+    :type y: Numpy ndarray
     """
-    assert X.shape[0] == l.shape[0]
-    assert l.shape[1] == 1
+    assert X.shape[0] == y.shape[0]
+    assert y.shape[1] == 1
     assert isinstance(X, np.ndarray)
-    assert isinstance(l, np.ndarray)
+    assert isinstance(y, np.ndarray)
 
-    A = X / l
+    A = X / y
     sumA = A.sum(axis=0)
     return A / sumA * 1e6
 
 
-def format_and_normalize(X, l):
+def format_and_normalize(X, y):
     """Validate Pandas DataFrame inputs, prepare Numpy arrays and normalize.
 
     :type X: Pandas DataFrame
-    :type l: Pandas DataFrame
+    :type y: Pandas DataFrame
     """
     assert isinstance(X, pd.DataFrame)
-    assert isinstance(l, pd.DataFrame)
+    assert isinstance(y, pd.DataFrame)
 
-    common_genes = X.index.intersection(l.index)
+    common_genes = X.index.intersection(y.index)
     X = X.loc[common_genes]
-    l = l.loc[common_genes]
+    y = y.loc[common_genes]
 
-    TPM = tpm_normalization(X.to_numpy(), l.to_numpy())
+    TPM = tpm_normalization(X.to_numpy(), y.to_numpy())
     return pd.DataFrame(TPM, index=X.index, columns=X.columns)
 
 
@@ -52,9 +52,13 @@ def main():
 
     """
     )
-    parser.add_argument('expressions_file', type=str, help='tab-delimited file with gene expression data (genes in rows, samples in cols)')
-    parser.add_argument('gene_lengths_file', type=str, help='tab-delimited file with gene lengths')
-    parser.add_argument('--output', '-o', type=str, help='output file name')
+    parser.add_argument(
+        "expressions_file",
+        type=str,
+        help="tab-delimited file with gene expression data (genes in rows, samples in cols)",
+    )
+    parser.add_argument("gene_lengths_file", type=str, help="tab-delimited file with gene lengths")
+    parser.add_argument("--output", "-o", type=str, help="output file name")
 
     args = parser.parse_args()
     expressions_path = args.expressions_file
@@ -92,7 +96,7 @@ def main():
         print("FEATURE_ID column not found in gene_lengths data")
         exit(1)
 
-    gene_lengths.set_index(feature_id_column, inplace = True)
+    gene_lengths.set_index(feature_id_column, inplace=True)
 
     if gene_lengths.shape[1] != 1:
         print("Gene lengths should have two columns: FEATURE_ID and GENE_LENGTHS")
@@ -101,6 +105,6 @@ def main():
     TPM = format_and_normalize(expressions, gene_lengths)
 
     if output_path is None:
-        print(TPM.to_csv(sep='\t'))
+        print(TPM.to_csv(sep="\t"))
     else:
-        TPM.to_csv(output_path, sep='\t')
+        TPM.to_csv(output_path, sep="\t")
