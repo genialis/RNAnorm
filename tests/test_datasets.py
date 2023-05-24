a034b4468daaefb5e094372812859067ee3886af
JureZmzrlikar <zmrzlikar.jure@gmail.com>
2023-05-24 16:32:27 +0200

    Ammend dataset loading code

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,13 +1,24 @@
+import pathlib
+
 import numpy as np
+import pandas as pd
 
 from rnanorm import TPM
-from rnanorm.datasets import load_example
+from rnanorm.datasets import load_rnaseq_toy
 
 
 def test_example():
-    """Test that datasets.load_example works as expected."""
-    example = load_example()
-    result = TPM(example.gtf_path).fit_transform(example.exp)
+    """Test that datasets.load_rnaseq_toy works as expected."""
+    # Numpy array output
+    ds = load_rnaseq_toy()
+    assert isinstance(ds.exp, np.ndarray)
+    assert isinstance(ds.gtf_path, pathlib.Path)
+
+    # Numpy array output
+    ds = load_rnaseq_toy(as_frame=True)
+    assert isinstance(ds.exp, pd.DataFrame)
+
+    result = TPM(ds.gtf_path).fit_transform(ds.exp)
 
     assert isinstance(result, np.ndarray)
     assert result.shape == (4, 5)
